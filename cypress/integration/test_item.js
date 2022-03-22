16c77c188606439b1a0f7169790d75415ae29a41
yadavyk <yashiyadav1998@gmail.com>
2022-03-22 10:49:14 +0530

    test(ui): document creation tests

--- cypress/integration/test_item.js
+++ cypress/integration/test_item.js
@@ -1,21 +1,29 @@
 context('Item', () => {
 	before(() => {
-		cy.login();
-	});
-	
-	it('Create an item', () => {
-		cy.visit(`app/item/`);
-        cy.get('.primary-action').click();
-        cy.get('.custom-actions > .btn').click();
+        cy.login();
+    });
+
+    it('Create an item', () => {
+        cy.visit(`app/item/`);
+		cy.click_listview_primary_button('Add Item');
+		cy.findByRole('button', {name: /Edit in full page/}).click();
         cy.get_field('item_code', 'Data').type("ITM-0018");
         cy.get_field('item_group', 'Link').type('All Item Groups');
+		cy.get_field('valuation_rate', 'Data').clear().type('8000');
+		cy.get_field('stock_uom', 'Link').clear().type('Nos');
         cy.wait(500);
-        cy.get(':nth-child(2) > form > [title="is_stock_item"] > .checkbox > label > .input-area > .input-with-feedback').click();
-        cy.get('.modal-footer > .standard-actions > .btn-primary').contains("Save").trigger('click', {force: true});
-		cy.get_field('item_code', 'Data').should('have.value','ITM-0018');
+		cy.findByRole('button', {name: 'Save'}).click();
+    });
+
+    it('Check Item attributes', () => {
+		cy.get('.page-title').should('contain', 'ITM-0018');
+		cy.get('.page-title').should('contain', 'Enabled');
+        cy.get_field('item_name', 'Data').should('have.value','ITM-0018');
         cy.get_field('item_group', 'Link').should('have.value', 'All Item Groups');
-        cy.get_field('stock_uom', 'Link').should('have.value','Nos');
-        cy.get('#page-Item > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
+		cy.get_field('is_stock_item', 'checkbox').should('be.checked');
+		cy.get_field('valuation_rate', 'Data').should('have.value', '8,000.00');
+		cy.get_field('stock_uom', 'Link').should('have.value','Nos');
         cy.remove_doc('Item', 'ITM-0018');
-	});
+    });
 });
+
