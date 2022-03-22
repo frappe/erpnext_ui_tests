1df844fd2760bde6593930200afb1c7d35c1929f
yadavyk <yashiyadav1998@gmail.com>
2022-03-22 11:30:20 +0530

    Changed test_supplier and test_lead

--- cypress/integration/test_supplier.js
+++ cypress/integration/test_supplier.js
@@ -8,12 +8,13 @@ context('Supplier', () => {
 		cy.visit(`app/supplier/`);
 		cy.get('.primary-action').click();
 		cy.get('.custom-actions > .btn').click();
-		cy.get_field('supplier_group', 'Link').type("All Supplier Groups");
+        cy.get_field('supplier_group', 'Link').type('All Supplier Groups').focus();
 		cy.get_field('supplier_name', 'Link').type("Medlink International Suppliers");
-		cy.get('#page-Supplier > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
+		cy.findByRole('button', {name: 'Save'}).click();
 		cy.get_field('supplier_name', 'Link').should('have.value', 'Medlink International Suppliers');
 		cy.get_field('supplier_group', 'Link').should('have.value', 'All Supplier Groups');	
-		cy.get('#page-Supplier > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
+        cy.location("pathname").should("not.be","/app/lead/new");
+        cy.get('.page-title').should('contain', 'Medlink International Suppliers');
 		cy.remove_doc('Supplier', 'Medlink International Suppliers');
 	});
 });
