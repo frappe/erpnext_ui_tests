9ce124ab6fb7d005b3a7aab61fbad9bab7197d4e
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-22 11:07:23 +0530

    Updated test_lead.js

--- cypress/integration/test_lead.js
+++ cypress/integration/test_lead.js
@@ -14,13 +14,12 @@ context('Lead', () => {
 		cy.get_field('last_name', 'Link').type("Eleanor");
 		cy.get_field('gender', 'Link').type("Male");
 		cy.get_field('email_id', 'Link').type("oliverel@gmail.com");
-		cy.get('#page-Lead > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
+		cy.findByRole('button', {name: 'Save'}).click();
 		cy.get_field('salutation', 'Link').should('have.value', 'Mr');
 		cy.get_field('company_name', 'Link').should('have.value', 'Eleanor School of Music');
 		cy.get_field('first_name', 'Link').should('have.value', 'Oliver');
 		cy.get_field('last_name', 'Link').should('have.value', 'Eleanor');
 		cy.get_field('email_id', 'Link').should('have.value', 'oliverel@gmail.com');
-		cy.get('#page-Lead > .page-head > .container > .row > .col > .standard-actions > .primary-action').click();
 		cy.remove_doc('Lead', 'CRM-LEAD-2022-00003');
 	});
 });
