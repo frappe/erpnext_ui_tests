90636d5a8d0bf8ca3c87209aae9d373c40e51b21
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-23 11:23:55 +0530

    Update test_supplier.js

--- cypress/integration/test_supplier.js
+++ cypress/integration/test_supplier.js
@@ -7,12 +7,12 @@ context('Supplier', () => {
 		cy.visit(`app/supplier/`);
 		cy.get('.primary-action').click();
 		cy.get('.custom-actions > .btn').click();
-        cy.get_field('supplier_group', 'Link').type('All Supplier Groups').focus();
+        cy.get_field('supplier_group', 'Link').clear().type('All Supplier Groups');
 		cy.get_field('supplier_name', 'Link').type("Medlink International Suppliers");
 		cy.findByRole('button', {name: 'Save'}).click();
 		cy.get_field('supplier_name', 'Link').should('have.value', 'Medlink International Suppliers');
 		cy.get_field('supplier_group', 'Link').should('have.value', 'All Supplier Groups');	
-        cy.location("pathname").should("not.be","/app/lead/new");
+        cy.location("pathname").should("not.be","/app/supplier/new");
         cy.get('.page-title').should('contain', 'Medlink International Suppliers');
 		cy.remove_doc('Supplier', 'Medlink International Suppliers');
 	});
