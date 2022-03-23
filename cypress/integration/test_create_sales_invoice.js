dcf3c24cef02763ea692bb19e3428c85ff914dca
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-03-23 18:14:44 +0530

    test: Sales Invoice Creation

---
+++ cypress/integration/test_create_sales_invoice.js
@@ -0,0 +1,69 @@
+
+context('Sales Invoice Creation', () => {
+	before(() => {
+		cy.login();
+	});
+	
+	it('Create Item', () => {
+		cy.visit('app/item');
+		cy.insert_doc(
+			"Item",
+			{
+				item_code: "Vintage Green Photo Frame",
+				item_group: "All Item Groups",
+				valuation_rate: 2000,
+				stock_uom: "Nos",
+			},
+			true
+		)
+	});
+
+	it('Create Customer', () => {
+		cy.insert_doc(
+			"Customer",
+			{
+				customer_name: "Anaya Kapoor",
+				customer_group: "All Customer Groups",
+				territory: "All Territories",
+			},
+			true
+		)
+	});
+
+	it('Create Sales Order', () => {
+		var today = new Date();
+		var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+		cy.insert_doc(
+			"Sales Order",
+			{
+				naming_series: "SAL-ORD-.YYYY.-",
+				transaction_date: date,
+				customer: "Anaya Kapoor",
+				order_type: "Sales",
+				items: [{"item_code": "Vintage Green Photo Frame", "delivery_date": date, "qty": 1, "rate": 2000}]
+			},
+			true
+		).then((d)=>{ 
+			console.log(d);
+			cy.visit('app/sales-order/'+ d.name);
+			cy.findByRole('button', {name: 'Submit'}).click();
+			cy.findByRole('button', {name: 'Yes'}).click();
+			cy.get('.page-title').should('contain', 'To Deliver and Bill');
+		});
+	});
+
+	it('Create Sales Invoice', () => {
+		cy.visit('app/sales-order/');
+		cy.click_listview_row_item(0);
+		cy.wait(1000);
+		cy.get('.form-documents > :nth-child(1) > :nth-child(1) > :nth-child(2) > .btn > .icon').click();  //Click on + icon on SO to open SI
+		cy.findByRole('button', {name: 'Save'}).click();
+		cy.get('.page-title').should('contain', 'Draft');
+		cy.wait(500);
+		cy.findByRole('button', {name: 'Submit'}).click();
+		cy.findByRole('button', {name: 'Yes'}).click();
+		cy.get('.page-title').should('contain', 'Anaya Kapoor');
+		cy.get('.page-title').should('contain', 'Unpaid');
+		cy.get('[data-fieldname="total"]').should('contain', '₹ 2,000.00');
+	});
+});
