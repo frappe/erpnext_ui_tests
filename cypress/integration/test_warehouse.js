4b72c2ae05400b82d28ef3e8822bf4d1082b8d65
yadavyk <yashiyadav1998@gmail.com>
2022-03-24 18:06:26 +0530

    modified last function

--- cypress/integration/test_warehouse.js
+++ cypress/integration/test_warehouse.js
@@ -43,14 +43,8 @@ context('Warehouse', () => {
     });
 
     it('Check if new button works', () => {
-	    cy.get('.primary-action').click();
-        cy.get_field('warehouse_name', 'Data').type("Reserve Warehouse - CT");
-        cy.get_field('warehouse_name', 'Data').should('have.value', 'Reserve Warehouse - CT');
-        //cy.get_field('company', 'Data').should('have.value', 'Campbell Tech');
-        //cy.get('.modal.show > .modal-dialog > .modal-content > .modal-footer > .standard-actions > .btn-primary').click();
-        cy.contains("Create New").trigger('click', {force: true});  //6ba3f2762e
-        cy.location("pathname").should("eq","/app/warehouse/tree");
+	    cy.get('.primary-action').click(); 
+        cy.get('.modal.show > .modal-dialog > .modal-content > .modal-header > .modal-actions > .btn-modal-close').click();
+        cy.location("pathname").should("eq","/app/warehouse/view/tree");
     });
-
-
 });
