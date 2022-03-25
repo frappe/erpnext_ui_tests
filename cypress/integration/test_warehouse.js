bf51f6219822581abc2afd1efd15278c49bf9299
yadavyk <yashiyadav1998@gmail.com>
2022-03-25 13:01:54 +0530

    changed test_warehouse.js

--- cypress/integration/test_warehouse.js
+++ cypress/integration/test_warehouse.js
@@ -5,20 +5,20 @@ context('Warehouse', () => {
 	});
 
 	it('Vist the warehouse tree view page', () => {
-		cy.visit(`app/warehouse/`);
+	cy.visit(`app/warehouse/`);
         cy.get('.custom-btn-group > .btn').click();
         cy.get('[data-view="Tree"] > .grey-link').click();
         cy.location("pathname").should("eq","/app/warehouse/view/tree");
     });
 
     it('Check if Expand Tree works', () => {
-		cy.visit(`app/warehouse/view/tree`);
+	cy.visit(`app/warehouse/view/tree`);
         cy.get('.custom-actions > .btn').click();
         cy.location("pathname").should("eq","/app/warehouse/view/tree");
     });
 
     it('Check if able to view list through menu', () => {
-		cy.visit(`app/warehouse/view/tree`);
+	cy.visit(`app/warehouse/view/tree`);
         cy.get('.custom-actions > .btn').click();
         cy.get('.menu-btn-group > .btn').click();
         cy.get(':nth-child(1) > .grey-link').click(); //Check if redirects to list view 
@@ -26,7 +26,7 @@ context('Warehouse', () => {
     });
 
     it('Check if print option in Menu button works', () => {
-		cy.visit(`app/warehouse/view/tree`);
+	cy.visit(`app/warehouse/view/tree`);
         cy.get('.custom-actions > .btn').click();
         cy.get('.menu-btn-group > .btn').click();
         cy.get(':nth-child(2) > .grey-link').click(); //Check if redirects to print
@@ -35,7 +35,7 @@ context('Warehouse', () => {
     });
 
     it('Check if refresh option in Menu button works', () => {
-		cy.visit(`app/warehouse/view/tree`);
+	cy.visit(`app/warehouse/view/tree`);
         cy.get('.custom-actions > .btn').click();
         cy.get('.menu-btn-group > .btn').click();
         cy.get(':nth-child(4) > .grey-link').click(); //Check if redirects to unexpand tree 
