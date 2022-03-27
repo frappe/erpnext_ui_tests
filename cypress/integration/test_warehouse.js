8c3430dff358086cca2a0f23740c6d452ff442ba
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-03-27 14:51:50 +0530

    Update test_warehouse.js

--- cypress/integration/test_warehouse.js
+++ cypress/integration/test_warehouse.js
@@ -43,10 +43,8 @@ context('Warehouse', () => {
     });
 
     it('Check if new button works', () => {
-	cy.visit(`app/warehouse/view/tree`);
-	cy.click_listview_primary_button('+ New');
-	cy.get_field('warehouse_name', 'Data').type('Reserve Warehouse', {delay: 200});
-	cy.contains('Create New').click();
+	cy.findByRole('button', {name: 'New'}).trigger('click', {force: true});
+        cy.visit(`app/warehouse/view/tree`);
         cy.location("pathname").should("eq","/app/warehouse/view/tree");
     });
 });
