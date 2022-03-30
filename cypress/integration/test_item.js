d3dd1d13ad24ab8635af514b2954e2a66592c771
Ankush Menat <ankush@frappe.io>
2022-03-30 13:19:23 +0530

    test: port item dashboard tests

--- cypress/integration/test_item.js
+++ cypress/integration/test_item.js
@@ -27,3 +27,47 @@ context("Item", () => {
 		cy.remove_doc("Item", "ITM-0018");
 	});
 });
+
+context("Item dashboard", () => {
+	before(() => {
+		cy.login();
+		cy.insert_doc(
+			"Item",
+			{
+				item_code: "e2e_test_item",
+				item_group: "All Item Groups",
+				opening_stock: 42,
+				valuation_rate: 100,
+			},
+			true
+		);
+		cy.go_to_doc("item", "e2e_test_item");
+	});
+
+	it("should show dashboard with correct data on first load", () => {
+		cy.findByText("Stock Levels").should("be.visible");
+		cy.get(".stock-levels").contains("e2e_test_item").should("exist");
+
+		// reserved and available qty
+		cy.get(".stock-levels .inline-graph-count")
+			.eq(0)
+			.contains("0")
+			.should("exist");
+		cy.get(".stock-levels .inline-graph-count")
+			.eq(1)
+			.contains("42")
+			.should("exist");
+	});
+
+	it("should persist on field change", () => {
+		cy.get('input[data-fieldname="disabled"]').check();
+		cy.wait(500);
+		cy.findByText("Stock Levels").should("be.visible");
+		cy.get(".stock-levels").should("have.length", 1);
+	});
+
+	it("should persist on reload", () => {
+		cy.reload();
+		cy.findByText("Stock Levels").should("be.visible");
+	});
+});
