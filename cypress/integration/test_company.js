dcd490d4aac1602fc68ebd4db24d37b5a147d902
yadavyk <yashiyadav1998@gmail.com>
2022-04-11 12:31:57 +0530

    verify if CoA and cost centers are created

--- cypress/integration/test_company.js
+++ cypress/integration/test_company.js
@@ -3,6 +3,7 @@ context('Create Company', () => {
     cy.login();
         });
 
+		//Create and validate attributes of company
         it('Create Company', () => {
             cy.visit(`app/company`);
             cy.wait(200);
@@ -16,5 +17,26 @@ context('Create Company', () => {
             cy.findByRole('button', {name: 'Save'}).trigger('click', {force: true});
             cy.wait(500);
             cy.get('.page-title').should('contain', 'Frappe Tech');
+
+			it("Check if appropriate Cost Centers are created", () => {
+			cy.findByRole("button", { name: "View" }).trigger("click", {
+				force: true,
+			});
+			cy.get('[data-label="Cost%20Centers"]').click({
+				delay: 200,
+			});
+			cy.location("pathname").should("eq", "/app/cost-center/view/tree");
         });
-    });
+
+			it("Check if appropriate Chart of Accounts are created", () => {
+			cy.findByRole("button", { name: "View" }).trigger("click", {
+				force: true,
+			});
+			cy.get('[data-label="Chart%20of%20Accounts"]').click({
+				delay: 200,
+			});
+			cy.location("pathname").should("eq", "/app/account/view/tree");
+		});
+	});
+});
+
