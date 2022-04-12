d1fcd944dd433d2fd5c2ee71e30a9ba3f8ee5ee0
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-04-12 19:27:22 +0530

    test: Corrected selectors

--- cypress/integration/control_data.js
+++ cypress/integration/control_data.js
@@ -42,9 +42,9 @@ context('Data Control', () => {
 		cy.get('.indicator-pill').should('have.text', 'Not Saved');
 
 		//Checking the labels of the data fields
-		cy.get('[title="name1"] > .form-group > .clearfix > .control-label').should('contain', 'Name');
-		cy.get('[title="email"] > .form-group > .clearfix > .control-label').should('contain', 'Email-ID');
-		cy.get('[title="phone"] > .form-group > .clearfix > .control-label').should('contain', 'Phone No.');
+		cy.get('[title="name1"]').contains('Name');
+		cy.get('[title="email"]').contains('Email-ID');
+		cy.get('[title="phone"]').contains('Phone No.');
 
 		//Inputting data in the field
 		cy.fill_field('name1', '@@###', 'Data');
