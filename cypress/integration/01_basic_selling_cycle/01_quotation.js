7568c64fec66790f2489902e364da80ed27c8eb4
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-04-12 18:02:43 +0530

    removed company creation

--- cypress/integration/01_basic_selling_cycle/01_quotation.js
+++ cypress/integration/01_basic_selling_cycle/01_quotation.js
@@ -1,149 +1,135 @@
 
 context('Quotation Creation', () => {
-	before(() => {
-		cy.login();
-		cy.visit('/app');
+    before(() => {
+        cy.login();
+        cy.visit('/app');
 
-			cy.insert_doc(
-				"Company",
-				{
-					"company_name": "Frappe Technologies Pvt. Ltd.",
-					"abbr": "FTPL",
-					"default_currency": "INR",
-					"country": "India",
-					"is_group": 0,
-					"create_chart_of_accounts_based_on": "Standard Template",
-					"chart_of_accounts": "India - Chart of Accounts",
-				},
-				true
-			)
+            cy.insert_doc(
+                "Item",
+                {
+                    item_code: "Apple iPhone 13 Pro",
+                    item_group: "All Item Groups",
+                    valuation_rate: 110000,
+                    stock_uom: "Nos",
+                },
+                true
+            )
 
-			cy.insert_doc(
-				"Item",
-				{
-					item_code: "Apple iPhone 13 Pro",
-					item_group: "All Item Groups",
-					valuation_rate: 110000,
-					stock_uom: "Nos",
-				},
-				true
-			)
+            cy.insert_doc(
+                "Customer",
+                {
+                    customer_name: "Maria Garcia",
+                    customer_group: "All Customer Groups",
+                    territory: "All Territories",
+                    default_currency: "INR",
+                    default_price_list: "Standard Selling",
+                },
+                true
+            )
 
-			cy.insert_doc(
-				"Customer",
-				{
-					customer_name: "Maria Garcia",
-					customer_group: "All Customer Groups",
-					territory: "All Territories",
-					default_currency: "INR",
-					default_price_list: "Standard Selling",
-				},
-				true
-			)
+            cy.insert_doc(
+                "Address",
+                {
+                    address_title: "Maria's Address",
+                    address_type: "Billing",
+                    address_line1: "18th Floor, ",
+                    address_line2: "Prabhat Bldg Off Sitladevi Temple Road, Vile Parle West, ",
+                    city: "Mumbai ",
+                    country: "India",
+                    "is_primary_address": 1,
+                    "is_shipping_address": 1,
+                    "links": [
+                        {
+                            "link_doctype": "Customer",
+                            "link_name": "Maria Garcia",
+                            "link_title": "Maria Garcia",
+                            "parent": "Maria's Address-Billing",
+                            "parentfield": "links",
+                            "parenttype": "Address",
+                            "doctype": "Dynamic Link"
+                        }
+                    ]
+                },
+                true
+            )
+    });
 
-			cy.insert_doc(
-				"Address",
-				{
-					address_title: "Maria's Address",
-					address_type: "Billing",
-					address_line1: "18th Floor, ",
-					address_line2: "Prabhat Bldg Off Sitladevi Temple Road, Vile Parle West, ",
-					city: "Mumbai ",
-					country: "India",
-					"is_primary_address": 1,
-					"is_shipping_address": 1,
-					"links": [
-						{
-							"link_doctype": "Customer",
-							"link_name": "Maria Garcia",
-							"link_title": "Maria Garcia",
-							"parent": "Maria's Address-Billing",
-							"parentfield": "links",
-							"parenttype": "Address",
-							"doctype": "Dynamic Link"
-						}
-					]
-				},
-				true
-			)
-	});
+    it('Create Quotation', () => {
+        cy.get('.avatar-frame').click();
+        cy.get('[onclick="return frappe.ui.toolbar.setup_session_defaults()"]').click();
+        cy.get_field('company', 'Link').focus().clear().trigger('click', {force: true});
+        cy.wait(400);
+        cy.get_field('company', 'Link').clear();
+        cy.fill_field("company", "Frappe Tech ", "Link"), {delay:200};
+        cy.get('[aria-selected="true"] > a > p').click();
+        cy.findByRole('button', {name: 'Save'}).trigger('click', {force: true});
 
-	it('Create Quotation', () => {
-		cy.get('.avatar-frame').click();
-		cy.get('[onclick="return frappe.ui.toolbar.setup_session_defaults()"]').click();
-		cy.get_field('company', 'Link').focus().clear().trigger('click', {force: true});
-		cy.wait(400);
-		cy.get_field('company', 'Link').clear();
-		cy.fill_field("company", "Frappe Technologies Pvt. Ltd. ", "Link"), {delay:200};
-		cy.get('[aria-selected="true"] > a > p').click();
-		cy.findByRole('button', {name: 'Save'}).trigger('click', {force: true});
+        cy.visit('app/quotation');
 
-		cy.visit('app/quotation');
+        var today = new Date();
+        const yyyy = today.getFullYear();
+        let mm = today.getMonth() + 1; // Months start at 0!
+        let nextMonth = today.getMonth() + 2; //Validiy till next month
+        let dd = today.getDate();
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
+        if (nextMonth < 10) nextMonth = '0' + nextMonth;
+        var today = dd + '-' + mm + '-' + yyyy;
+        var validTill = dd + '-' + nextMonth + '-' + yyyy;
 
-		var today = new Date();
-		const yyyy = today.getFullYear();
-		let mm = today.getMonth() + 1; // Months start at 0!
-		let nextMonth = today.getMonth() + 2; //Validiy till next month
-		let dd = today.getDate();
-		if (dd < 10) dd = '0' + dd;
-		if (mm < 10) mm = '0' + mm;
-		if (nextMonth < 10) nextMonth = '0' + nextMonth;
-		var today = dd + '-' + mm + '-' + yyyy;
-		var validTill = dd + '-' + nextMonth + '-' + yyyy;
+        cy.click_listview_primary_button('Add Quotation');
+        cy.url().should('include', '/quotation/new-quotation');
 
-		cy.click_listview_primary_button('Add Quotation');
-		cy.url().should('include', '/quotation/new-quotation');
+        cy.get_field('naming_series', 'Select').should('have.value', 'SAL-QTN-.YYYY.-');
+        cy.wait(200);
+        cy.get_field('company', 'Link').focus().clear().trigger('click', {force: true});
+        cy.wait(400);
+        cy.get_field('company', 'Link').clear();
+        cy.fill_field("company", "Frappe Tech", "Link"), {delay:200};
+        cy.get('[aria-selected="true"]').first();
+        //cy.get('[aria-selected="true"] > a > p > strong').click();
 
-		cy.get_field('naming_series', 'Select').should('have.value', 'SAL-QTN-.YYYY.-');
-		cy.wait(200);
-		cy.get_field('company', 'Link').focus().clear().trigger('click', {force: true});
-		cy.wait(400);
-		cy.get_field('company', 'Link').clear();
-		cy.fill_field("company", "Frappe Technologies Pvt. Ltd. ", "Link"), {delay:200};
-		cy.get('[aria-selected="true"]').first();
-		//cy.get('[aria-selected="true"] > a > p > strong').click();
+        cy.get_field('transaction_date', 'Date').should('have.value', today);
+        cy.get_field('quotation_to', 'Link').should('have.value', "Customer");
+        cy.get_field('valid_till', 'Date').should('have.value', validTill);
+        cy.get_field('party_name', 'Dynamic Link').focus().trigger('click', {force: true});
+        cy.wait(500);
+        cy.fill_field('party_name', 'Maria Garcia', 'Dynamic Link'), {delay:200}, "{downarrow}{enter}";
+        cy.get('#awesomplete_list_6 > [aria-selected="true"]').first().click();
+        //cy.get('#awesomplete_list_6 > [aria-selected="true"] > a > p > strong').contains('Maria Garcia').click();
+        cy.get_field('order_type', 'Select').should('have.value', "Sales");
+        cy.get('[data-fieldname="customer_name"]').should('contain', "Maria Garcia");
 
-		cy.get_field('transaction_date', 'Date').should('have.value', today);
-		cy.get_field('quotation_to', 'Link').should('have.value', "Customer");
-		cy.get_field('valid_till', 'Date').should('have.value', validTill);
-		cy.get_field('party_name', 'Dynamic Link').focus().trigger('click', {force: true});
-		cy.wait(500);
-		cy.fill_field('party_name', 'Maria Garcia', 'Dynamic Link'), {delay:200}, "{downarrow}{enter}";
-		cy.get('#awesomplete_list_6 > [aria-selected="true"]').first().click();
-		//cy.get('#awesomplete_list_6 > [aria-selected="true"] > a > p > strong').contains('Maria Garcia').click();
-		cy.get_field('order_type', 'Select').should('have.value', "Sales");
-		cy.get('[data-fieldname="customer_name"]').should('contain', "Maria Garcia");
+        cy.findByText('Address and Contact').click();
+        cy.get('[title="customer_address"]').should('contain', "Maria's Address-Billing");
+        cy.get('[title="shipping_address_name"]').should('contain', "Maria's Address-Billing");
+        cy.get_field('territory', 'Link').should('have.value', 'All Territories');
 
-		cy.findByText('Address and Contact').click();
-		cy.get('[title="customer_address"]').should('contain', "Maria's Address-Billing");
-		cy.get('[title="shipping_address_name"]').should('contain', "Maria's Address-Billing");
-		cy.get_field('territory', 'Link').should('have.value', 'All Territories');
+        cy.findByText('Currency and Price List').click();
+        cy.get_field('currency', 'Link').should('have.value', "INR");
+        cy.get_field('selling_price_list', 'Link').should('have.value', "Standard Selling");
 
-		cy.findByText('Currency and Price List').click();
-		cy.get_field('currency', 'Link').should('have.value', "INR");
-		cy.get_field('selling_price_list', 'Link').should('have.value', "Standard Selling");
+        cy.get('.rows > .grid-row > .data-row > .col-xs-4').trigger('click', {force: true});
+        cy.get_field('item_code', 'Link').focus().trigger('click', {force: true});
+        cy.wait(500);
+        cy.fill_field('item_code', 'Apple iPhone 13 Pro', 'Link'), {delay:200}, "{downarrow}{enter}";
+        cy.get('#awesomplete_list_27 > [aria-selected="true"] > a > p').contains('Apple iPhone 13 Pro').click();
+        cy.get_field('qty', 'Float').should('have.value', "1.000");
+        cy.get_field('rate', 'Float').clear();
+        cy.fill_field('rate', '110000', 'Float'), {delay:200}, "{enter}";
+        cy.get_field('rate', 'Float').blur();
+        cy.get('[data-fieldname="amount"]').should('contain', "1,10,000.00");
+        cy.get('[data-fieldname="total_qty"]').should('contain', "1");
+        cy.get('[data-fieldname="total"]').should('contain', "₹ 1,10,000.00");
 
-		cy.get('.rows > .grid-row > .data-row > .col-xs-4').trigger('click', {force: true});
-		cy.get_field('item_code', 'Link').focus().trigger('click', {force: true});
-		cy.wait(500);
-		cy.fill_field('item_code', 'Apple iPhone 13 Pro', 'Link'), {delay:200}, "{downarrow}{enter}";
-		cy.get('#awesomplete_list_27 > [aria-selected="true"] > a > p').contains('Apple iPhone 13 Pro').click();
-		cy.get_field('qty', 'Float').should('have.value', "1.000");
-		cy.get_field('rate', 'Float').clear();
-		cy.fill_field('rate', '110000', 'Float'), {delay:200}, "{enter}";
-		cy.get_field('rate', 'Float').blur();
-		cy.get('[data-fieldname="amount"]').should('contain', "1,10,000.00");
-		cy.get('[data-fieldname="total_qty"]').should('contain', "1");
-		cy.get('[data-fieldname="total"]').should('contain', "₹ 1,10,000.00");
+        cy.get('[data-fieldname="grand_total"]').should('contain', "₹ 1,10,000.00");
+        cy.get('[data-fieldname="rounded_total"]').should('contain', "₹ 1,10,000.00");
 
-		cy.get('[data-fieldname="grand_total"]').should('contain', "₹ 1,10,000.00");
-		cy.get('[data-fieldname="rounded_total"]').should('contain', "₹ 1,10,000.00");
+        cy.findByRole('button', {name: 'Save'}).trigger('click', {force: true});
+        cy.get('.page-title').should('contain', 'Draft');
+        cy.findByRole('button', {name: 'Submit'}).click();
+        cy.findByRole('button', {name: 'Yes'}).click();
+        cy.get('.page-title').should('contain', 'Open');
 
-		cy.findByRole('button', {name: 'Save'}).trigger('click', {force: true});
-		cy.get('.page-title').should('contain', 'Draft');
-		cy.findByRole('button', {name: 'Submit'}).click();
-		cy.findByRole('button', {name: 'Yes'}).click();
-		cy.get('.page-title').should('contain', 'Open');
-
-	});
+    });
 });
