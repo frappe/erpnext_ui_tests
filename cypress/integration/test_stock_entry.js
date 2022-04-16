7bbeeb8ef1dd734906ff6c55adc3536a1b5ae05b
yadavyk <yashiyadav1998@gmail.com>
2022-04-16 14:44:22 +0530

    changes

--- cypress/integration/test_stock_entry.js
+++ cypress/integration/test_stock_entry.js
@@ -12,7 +12,7 @@ context('Create Stock Entry', () => {
 			cy.get('.frappe-control[data-fieldname="items"]').as('table');
 			cy.get('@table').find('[data-idx="1"]').as('row1');
 			cy.get('@row1').find('.btn-open-row').click();
-            cy.wait(500);
+
 
 			//Set items table attributes
 			cy.get_field('s_warehouse', 'Link').scrollIntoView().should('be.visible').click({force:true});
@@ -23,18 +23,21 @@ context('Create Stock Entry', () => {
 			cy.get_field('item_code', 'Link').focus();
 			cy.get_field('item_code', 'Link').type('ITM-0001', {delay: 200});
 			cy.get_field('item_code', 'Link').should('have.value', 'ITM-0001');
-			cy.wait(500);
-			cy.get_field('qty', 'Link').scrollIntoView().should('be.visible').click({force:true});
 			cy.get_field('qty', 'Link').focus();
-			cy.get_field('qty', 'Float').clear().type('2');
+			cy.get_field('qty', 'Link').scrollIntoView().should('be.visible').click({force:true});
+			cy.get_field('qty', 'Float').clear().type('23.000');
 			cy.get_field('uom', 'Link').clear().type('Nos');
 			cy.get_field('uom', 'Link').should('have.value', 'Nos');
 			cy.get('.form-area > .form-layout > .form-page > :nth-child(5)').click();
-			cy.get_field('conversion_factor', 'Float').type('1');
-			cy.get_field('conversion_factor', 'Link').should('have.value', '1');
+			cy.get_field('conversion_factor', 'Float').type('1.000');
+			cy.get_field('conversion_factor', 'Link').should('have.value', '1.000');
 
 			//check amount and totals
+			cy.get_field('basic_amount', 'Link').focus();
+			cy.get_field('basic_amount', 'Link').scrollIntoView().should('be.visible').click({force:true});
 			cy.get_field('basic_amount', 'Link').should('not.have.value','0');
+			cy.get_field('amount', 'Link').focus();
+			cy.get_field('amount', 'Link').scrollIntoView().should('be.visible').click({force:true});
 			cy.get_field('amount', 'Link').should('not.have.value','0');
 			cy.get('.grid-collapse-row').click();
 			cy.get_field('total_incoming_value').should('not.have.value','0');
