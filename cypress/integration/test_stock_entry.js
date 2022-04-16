ed6c3ff53ef58281f96ac1a31fd368754d5adce4
yadavyk <yashiyadav1998@gmail.com>
2022-04-16 14:01:03 +0530

    fixes: test_stock_entry.js

--- cypress/integration/test_stock_entry.js
+++ cypress/integration/test_stock_entry.js
@@ -13,6 +13,8 @@ context('Create Stock Entry', () => {
 			cy.get('@table').find('[data-idx="1"]').as('row1');
 			cy.get('@row1').find('.btn-open-row').click();
             cy.wait(500);
+
+			//Set items table attributes
 			cy.get_field('s_warehouse', 'Link').scrollIntoView().should('be.visible').click({force:true});
 			cy.get_field('s_warehouse', 'Link').focus();
 			cy.get_field('s_warehouse', 'Link').type('Stores - CT', {delay: 100});
@@ -30,7 +32,13 @@ context('Create Stock Entry', () => {
 			cy.get('.form-area > .form-layout > .form-page > :nth-child(5)').click();
 			cy.get_field('conversion_factor', 'Float').type('1');
 			cy.get_field('conversion_factor', 'Link').should('have.value', '1');
+
+			//check amount and totals
+			cy.get_field('basic_amount', 'Link').should('not.have.value','0');
+			cy.get_field('amount', 'Link').should('not.have.value','0');
 			cy.get('.grid-collapse-row').click();
+			cy.get_field('total_incoming_value').should('not.have.value','0');
+
 			cy.findByRole('button', {name: 'Update Rate and Availability'}).trigger('click', {force: true});
 			cy.findByRole('button', {name: 'Save'}).trigger('click', {force: true});
 			cy.findByRole('button', {name: 'Submit'}).trigger('click', {force: true});
