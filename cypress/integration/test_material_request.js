527df9149f417080867cbfe803edfd9591ddb009
yadavyk <yashiyadav1998@gmail.com>
2022-04-22 23:24:11 +0530

    changes

--- cypress/integration/test_material_request.js
+++ cypress/integration/test_material_request.js
@@ -3,7 +3,39 @@ context('Material Request', () => {
     cy.login();
         });
 
-			//Set mode of payment first
+		it('Create an item', () => {
+			cy.new_doc_view('Item');
+
+			cy.get_field('item_code', 'Data').type('ITM-0001');
+			cy.get_field('item_group', 'Link').clear().type('All Item Groups');
+			cy.get_field('opening_stock', 'Data').clear().type(100);
+			cy.get_field('standard_rate', 'Data').clear().type(100);
+			cy.get_field('stock_uom', 'Link').clear().type("Nos");
+
+			cy.get('.frappe-control[data-fieldname="item_defaults"]').as('table');
+			cy.get('@table').findByRole('button', {name: 'Add Row'}).click();
+			cy.get('@table').find('[data-idx="1"]').as('row1');
+			cy.get('@row1').find('.btn-open-row').click();
+			cy.get_field('company', 'Link').clear().type('Wind Power LLC', {delay: 200});
+			cy.get_field('company', 'Link').should('have.value','Wind Power LLC');
+			cy.get_field('default_warehouse', 'Link').clear().type('Stores - WPL', {delay: 200});
+			cy.get_field('default_warehouse', 'Link').should('have.value','Stores - WPL');
+			cy.get('.grid-collapse-row').click();
+			cy.findByRole("button", { name: "Save" }).click();
+			cy.wait(500);
+
+			cy.get(".page-title").should("contain", "ITM-0001");
+			cy.get(".page-title").should("contain", "Enabled");
+
+			cy.compare_document({
+				item_name: 'ITM-0001',
+				standard_rate: '100',
+				item_group: 'All Item Groups',
+				description: 'ITM-0001',
+				stock_uom: 'Nos',
+				is_stock_item: 1,
+		});
+	});
 			it('Set appropriate field values', () => {
             cy.visit(`app/material-request`);
             cy.click_listview_primary_button('Add Material Request');
