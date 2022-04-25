cf2d7da7dc257c4748d1f98c2e146256b2f5c63f
yadavyk <yashiyadav1998@gmail.com>
2022-04-25 10:55:52 +0530

    changes

--- cypress/integration/test_material_request.js
+++ cypress/integration/test_material_request.js
@@ -3,7 +3,7 @@ context('Material Request', () => {
     cy.login();
         });
 
-		it('Create an item', () => {
+		/*it('Create an item', () => {
 			cy.new_doc_view('Item');
 
 			cy.get_field('item_code', 'Data').type('ITM-0001');
@@ -35,7 +35,7 @@ context('Material Request', () => {
 				stock_uom: 'Nos',
 				is_stock_item: 1,
 		});
-	});
+	});*/
 			it('Set appropriate field values', () => {
             cy.visit(`app/material-request`);
             cy.click_listview_primary_button('Add Material Request');
@@ -46,13 +46,18 @@ context('Material Request', () => {
 
 			//Setting Required By field
 			var today = new Date();
-            var date = '01-'+(today.getMonth()+2)+'-'+today.getFullYear();
+            var date = '01-'+(today.getMonth()+3)+'-'+today.getFullYear();
 			cy.get_field('schedule_date', 'Date').wait(500).clear().type(date, {delay: 200});
 
 			cy.get(':nth-child(3) > .section-body > :nth-child(1)').click();
 			cy.get_field('company', 'Link').clear().type('Wind Power LLC', {delay: 200}, "{downarrow}{enter}");
-
-			//Set items table attributes
+			cy.get('.rows > .grid-row > .data-row > [data-fieldname="item_code"]').click();
+			cy.get_field('item_code', 'Link').type('ITM-0001', {delay: 200});
+			cy.get_field('item_code', 'Link').should('have.value', 'ITM-0001');
+			cy.get_field('schedule_date', 'Date').should('not.have.value','');
+			cy.get_field('qty', 'Float').clear().type('23.000');
+			cy.get_field('uom', 'Link').should('have.value', 'Nos');
+			/*//Set items table attributes
 			cy.get('.frappe-control[data-fieldname="items"]').as('table');
 			cy.get('@table').find('[data-idx="1"]').as('row1');
 			cy.get('@row1').find('.btn-open-row').click();
@@ -63,7 +68,7 @@ context('Material Request', () => {
 			//cy.get_field('description', 'Link').type('ITM-0001', {delay: 200});
 			cy.get_field('schedule_date', 'Date').should('not.have.value','');
 			cy.get_field('qty', 'Float').clear().type('23.000');
-			cy.get_field('uom', 'Link').clear().type('Nos');
+			//cy.get_field('uom', 'Link').clear().type('Nos');
 			cy.get_field('uom', 'Link').should('have.value', 'Nos');
 			cy.get_field('warehouse', 'Link').should('have.value', 'Stores - WPL');
 			cy.get_field('conversion_factor', 'Float').clear().type('1');
@@ -75,9 +80,9 @@ context('Material Request', () => {
 			cy.get_field('rate', 'Link').type('100');
 			cy.get_field('rate', 'Link').should('not.have.value','0');
 			//cy.get_field('amount', 'Link').focus().trigger('click', {force: true});
-			//cy.get_field('amount', 'Link').should('not.have.value','0');
+			//cy.get_field('amount', 'Link').should('not.have.value','0');*/
 
-			cy.get('.grid-collapse-row').click();
+			//cy.get('.grid-collapse-row').click();
 			cy.findByRole('button', {name: 'Save'}).trigger('click', {force: true});
 			cy.findByRole('button', {name: 'Submit'}).trigger('click', {force: true});
 			cy.findByRole('button', {name: 'Yes'}).trigger('click', {force: true});
