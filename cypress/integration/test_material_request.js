827aeff1ac0c9f0498666a3963af6206c92a5052
yadavyk <yashiyadav1998@gmail.com>
2022-04-25 11:40:30 +0530

    changes

--- cypress/integration/test_material_request.js
+++ cypress/integration/test_material_request.js
@@ -3,7 +3,7 @@ context('Material Request', () => {
     cy.login();
         });
 
-		/*it('Create an item', () => {
+		it('Create an item', () => {
 			cy.new_doc_view('Item');
 
 			cy.get_field('item_code', 'Data').type('ITM-0001');
@@ -35,7 +35,7 @@ context('Material Request', () => {
 				stock_uom: 'Nos',
 				is_stock_item: 1,
 		});
-	});*/
+	});
 			it('Set appropriate field values', () => {
             cy.visit(`app/material-request`);
             cy.click_listview_primary_button('Add Material Request');
