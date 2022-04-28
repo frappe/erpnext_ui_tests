3d20be3786226b181d43832637c90c61ce07be15
yadavyk <yashiyadav1998@gmail.com>
2022-04-28 16:08:45 +0530

    changes to company.js

--- cypress/integration/accounts/company.js
+++ cypress/integration/accounts/company.js
@@ -5,24 +5,22 @@ context('Create Company', () => {
 
 	it('Create Company', () => {
 		cy.new_doc('Company');
-		cy.set_input('company_name', 'Frappe Tech');
-		cy.get_field('abbr', 'Data').should('have.value', 'FT');
+		cy.set_input('company_name', 'Bernhardt Furnitures');
+		cy.get_field('abbr', 'Data').should('have.value', 'BF');
 		cy.set_link('default_currency', 'INR');
 		cy.set_link('country', 'India');
 		cy.click_toolbar_button('Save');
 		cy.wait(5000);
-		cy.get_page_title().should('contain', 'Frappe Tech');
+		cy.get_page_title().should('contain', 'Bernhardt Furnitures');
 	});
 
 	it("Check if appropriate Cost Centers are created", () => {
-		cy.click_toolbar_button('View');
-		cy.click_toolbar_dropdown('Cost Centers');
+		cy.click_dropdown_action('View','Cost Centers');
 		cy.location("pathname").should("eq", "/app/cost-center/view/tree");
 	});
 
 	it("Check if appropriate Chart of Accounts are created", () => {
-		cy.click_toolbar_button('View');
-		cy.click_toolbar_dropdown('Chart of Accounts');
+		cy.click_dropdown_action('View','Chart of Accounts');
 		cy.location("pathname").should("eq", "/app/account/view/tree");
 	});
 });
