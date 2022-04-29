2d9c456b372e8082879075c35e9ea28ef4173eea
Yashi Yadav <32797974+yadavyk@users.noreply.github.com>
2022-04-29 18:50:29 +0530

    test(ui) : stock settings (#30)
    * test(ui) : stock settings
    
    * modifications
    
    * changes
    
    * changing selector
    
    * field handling changes
    
    * changed selector
    
    * changes
    
    * discarding selectors
    
    * warehouse field changes
    
    * test: stock settings
    
    * refactor with new commands
    
    * replaced with new commands

--- cypress/integration/test_stock_settings.js
+++ cypress/integration/test_stock_settings.js
@@ -4,32 +4,31 @@ context("Stock Settings", () => {
 	});
 
 	it("Set Item Defaults", () => {
-		cy.visit(`app/stock-settings`);
+		cy.new_doc('Stock Settings');
 		cy.get_field('item_naming_by', 'Select').select('Item Code');
 		cy.get_field('item_naming_by', 'Select').should('have.value', 'Item Code');
-		cy.get_field('default_warehouse', 'Link').clear().type('Stores - WP', {delay: 200});
-		cy.get_field('default_warehouse', 'Link').should('have.value','Stores - WP');
-		cy.get_field('item_group', 'Link').clear().type('All Item Groups', {delay: 200});
-		cy.get_field('item_group', 'Link').should('have.value','All Item Groups');
-		cy.get_field('stock_uom', 'Link').clear().type('Nos', {delay: 200});
-		cy.get_field('stock_uom', 'Link').should('have.value','Nos');
+		cy.set_link('default_warehouse', 'Stores - WP');
+		cy.set_link('item_group', 'All Item Groups');
+        cy.get_input('item_group', 'Link').should('have.value', 'Nos');
+		cy.set_link('stock_uom', 'Nos');
+	    cy.get_input('uom', 'Data').should('have.value', 'Nos');
 		cy.contains('Stock Validations').click();
 		cy.get_field('show_barcode_field', 'checkbox').check();
-		cy.get_field('show_barcode_field', 'checkbox').should('be.checked');
+		cy.get_input('show_barcode_field', 'checkbox').should('be.checked');
 		cy.wait(500);
-		cy.findByRole("button", { name: 'Save' }).trigger("click", {force: true});
+		cy.save();
 	});
 
 	it("Check if all options set under item defaults are working", () => {
-	    cy.visit(`app/item/new-item-1`);
-		cy.get_field("item_code", "Data").type('ITM-0005', {delay: 200});
+	    cy.new_doc('Item');
+		cy.set_input('item_code', 'Teak 3-Sides Glass Cabinet');
 
 		//Check if Item Group is set as per item defaults in stock settings
-		cy.get_field('item_group', 'Link').should('have.value','All Item Groups');
+		cy.get_input('item_group', 'Link').should('have.value','All Item Groups');
 
 		//Check if Stock UOM is set as per item defaults in stock settings
-		cy.get_field('stock_uom', 'Link').should('have.value','Nos');
-		cy.get_field('standard_rate', 'Currency').type('123.00', {delay: 200});
+		cy.get_input('stock_uom', 'Link').should('have.value','Nos');
+		cy.set_input('standard_rate', '123.000');
 
 		//Check for default warehouse : if it's the same as set as per Stock Settings
 		cy.get('.frappe-control[data-fieldname="item_defaults"]').as('table');
@@ -38,25 +37,26 @@ context("Stock Settings", () => {
  		cy.get('@row1').find('.btn-open-row').click();
 
 		//cy.get_field('company', 'Link').clear().type('Wind Power LLC', {delay: 200});
-		cy.get_field('company', 'Link').should('have.value','Wind Power LLC');
-		cy.get_field('default_warehouse', 'Link').clear().type('Stores - WP');
-		cy.get_field('default_warehouse', 'Link').should('have.value','Stores - WP');
+		cy.get_input('company', 'Link').should('have.value','Wind Power LLC');
+		cy.set_input('default_warehouse', 'Stores - WP');
+		cy.get_input('default_warehouse', 'Link').should('have.value','Stores - WP');
         cy.get('.grid-collapse-row').click();
-		cy.findByRole("button", { name: "Save" }).click();
+		cy.save();
 
 		//Check if item naming is done as per Item Code
-		cy.get(".page-title").should("contain", "ITM-0005");
+		cy.get_page_title().should('contain', 'Teak 3-Sides Glass Cabinet');
+ 		cy.get_page_title().should('contain',  'Enabled');
 	});
 
 	it("Check if Item Price has been inserted as per standard selling rate as auto insert price list option was unchecked", () => {
-		cy.visit(`app/item-price`);
+	    cy.new_doc('Item Price');
         cy.click_listview_row_item(0);
-		cy.get_field('price_list_rate', 'Currency').should('not.have.value','0');
-		cy.remove_doc("Item", "ITM-0005");
+	    cy.get_input('price_list_rate', 'Currency').should('not.have.value','0');
+	    cy.remove_doc("Item", "Pen");
 	});
 
 	it("Check if barcode field appears in stock transactions ", () => {
-			cy.visit(`app/purchase-receipt/new-purchase-receipt-1`);
+			cy.new_doc
 			cy.get('.frappe-control[data-fieldname="items"]').as('table');
         	cy.get('@table').find('[data-idx="1"]').as('row1');
         	cy.get('@row1').find('.btn-open-row').click();
