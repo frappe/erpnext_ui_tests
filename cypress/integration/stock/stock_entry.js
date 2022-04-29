5590491f776af236ca54e41fae5d296e1f8f379d
yadavyk <yashiyadav1998@gmail.com>
2022-04-29 15:07:06 +0530

    refator with new commands

--- cypress/integration/stock/stock_entry.js
+++ cypress/integration/stock/stock_entry.js
@@ -3,41 +3,7 @@ context('Create Stock Entry', () => {
     cy.login();
         });
 
-		it('Create an item', () => {
-			cy.new_doc('Item');
-			cy.set_input('item_code', 'T-shirt');
-			cy.set_link('item_group','All Item Groups');
-			cy.set_input('opening_stock','100');
-			cy.set_input('standard_rate','100');
- 			cy.set_input('valuation_rate', '8000');
- 			cy.set_link('stock_uom', 'Nos');
-
-			cy.get('.frappe-control[data-fieldname="item_defaults"]').as('table');
-			cy.get('@table').findByRole('button', {name: 'Add Row'}).click();
-			cy.get('@table').find('[data-idx="1"]').as('row1');
-			cy.get('@row1').find('.btn-open-row').click();
-			cy.get_field('company', 'Link').clear().type('Wind Power LLC', {delay: 200});
-			cy.get_field('company', 'Link').should('have.value','Wind Power LLC');
-			cy.get('.form-area > .form-layout > .form-page > :nth-child(1)').click();
-			cy.set_link('company', 'Wind Power LLC');
-			cy.get_input('company', 'Link').should('have.value','Wind Power LLC');
-			cy.get('.grid-collapse-row').click();
-			cy.save();
-			cy.wait(500);
-
-			cy.get_page_title().should('contain', 'T-shirt');
- 			cy.get_page_title().should('contain',  'Enabled');
-
-			cy.compare_document({
-				item_name: 'T-shirt',
-				standard_rate: '100',
-				item_group: 'All Item Groups',
-				stock_uom: 'Nos',
-				is_stock_item: 1,
-		});
-	});
-
-			it('Set Item Table in Material Request', () => {
+			it.only('Set Item Table in Material Request', () => {
 			cy.new_doc('Stock Entry');
 			cy.location("pathname").should("eq","/app/stock-entry/new-stock-entry-1");
 			cy.get_field('naming_series', 'Select').select('MAT-STE-.YYYY.-');
@@ -51,65 +17,15 @@ context('Create Stock Entry', () => {
 			//Set warehouse
 			cy.set_link('to_warehouse', 'Finished Goods - WP');
 
-			cy.get_field('company', 'Link').clear().type('Wind Power LLC', {delay: 200}, "{downarrow}{enter}");
-
-			cy.get_field('to_warehouse', 'Link').type('Finished Goods - WPL', {delay: 200}, "{downarrow}{enter}");
-			//Set items table attributes
-			cy.get('.frappe-control[data-fieldname="items"]').as('table');
-			cy.get('@table').find('[data-idx="1"]').as('row1');
-			cy.get('@row1').find('.btn-open-row').click();
-
-			cy.get_field('item_code', 'Link').type('ITM-0011', {delay: 200});
-			cy.get_field('item_code', 'Link').should('have.value', 'ITM-0011');
-
-			cy.get_field('uom', 'Link').clear().type('Nos');
-			cy.get_field('uom', 'Link').should('have.value', 'Nos');
-			cy.get_field('conversion_factor', 'Float').type('1', {delay: 100});
-			cy.get_field('conversion_factor', 'Link').should('have.value', '1');
-			cy.get_field('allow_zero_valuation_rate', 'Check').check();
-
-			cy.get_field('qty', 'Float').scrollIntoView().should('be.visible')
-				.click({force:true})
-				.clear().type('23.000') ;
-
-			cy.get('.form-area > .form-layout > .form-page > :nth-child(5)').click();
-
-			//check amount and totals
-			cy.get('[data-fieldname="basic_amount"]').should('not.have.value','0');
-			cy.get('.grid-collapse-row').click();
-
-			cy.get_field('to_warehouse', 'Link').type('Finished Goods - WP', {delay: 200}, "{downarrow}{enter}");
-
-			//Set items table attributes
-			cy.get('.rows > .grid-row > .data-row > [data-fieldname="s_warehouse"]').click();
-			cy.set_input('item_code', 'T-shirt');
-			cy.get_field('item_code', 'Link').should('have.value', 'T-shirt');
-
-			cy.get('.frappe-control[data-fieldname="items"]').as('table');
-			cy.get('@table').find('[data-idx="1"]').as('row1');
-			cy.get('@row1').find('.btn-open-row').click();
-			cy.set_input('qty', '23.000');
-			cy.get_field('qty', 'Float').should('have.value','23.000');
-			cy.get('.grid-collapse-row').click();
-			cy.wait(500);
-
-			cy.findByRole('button', {name: 'Update Rate and Availability'}).trigger('click', {force: true});
+			cy.set_link('items.item_code', 'Birch Ply');
+			cy.set_input('items.qty', 10);
+			cy.grid_add_row('items');
+			cy.set_link('items.item_code', 'WB-001');
+			cy.set_input('items.qty', 40);
 			cy.save();
 			cy.wait(500);
-			cy.get('[data-fieldname="total_incoming_value"]').should('not.have.value','0');
-
-			cy.findByRole('button', {name: 'Submit'}).trigger('click', {force: true});
-			cy.findByRole('button', {name: 'Yes'}).trigger('click', {force: true});
-            cy.wait(500);
-			cy.get('.page-title').should('contain', 'Submitted');
-			cy.get_open_dialog().find('.btn-modal-close').click();
-			cy.get('.modal:visible').should('not.exist');
-
-			cy.click_toolbar_button('Submit');
-			cy.click_modal_primary_button('Yes');
-            cy.get_open_dialog().find('.btn-modal-close').click();
-			cy.get('.modal:visible').should('not.exist');
-			cy.get_page_title().should('contain',  'Submitted');
+			cy.submit('Submitted');
+			cy.wait(500);
 
 			//View Stock Ledger and verify if correct stock ledger entries are correct
 			cy.url().then((url) => {
