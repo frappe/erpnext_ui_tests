f591535b8fb8da834d67de80e229236471f39db0
yadavyk <yashiyadav1998@gmail.com>
2022-05-02 17:53:39 +0530

    changes to quick_stock_balance

--- cypress/integration/stock/quick_stock_balance.js
+++ cypress/integration/stock/quick_stock_balance.js
@@ -8,7 +8,7 @@ context('Quick Stock Balance', () => {
 		cy.set_link('warehouse', 'Stores - WP');
 		cy.get_field('warehouse', 'Link').should('have.value','Stores - WP');
 		cy.set_today('date');
-		cy.set_input('item', 'Teak Shoe Rack');
+		cy.set_link('item', 'Teak Shoe Rack');
 		cy.get_read_only('qty').should('contain','100');
 		cy.get_read_only('item_name').should('contain','Teak Shoe Rack');
 		cy.get_read_only('item_description').should('contain','Teak Shoe Rack');
