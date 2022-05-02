2a2286bdcce2fb63763688ee6bbfe862712896be
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-05-02 16:00:14 +0530

    test(refactor): sales_invoice_payment

--- cypress/integration/selling/sales_invoice_payment.js
+++ cypress/integration/selling/sales_invoice_payment.js
@@ -1,34 +1,43 @@
-
 context('Sales Invoice Payment', () => {
 	before(() => {
 		cy.login();
 		cy.visit('/app');
-			cy.insert_doc(
-				"Item",
-				{
-					item_code: "Sleepy Owl Coffee Mug",
-					item_group: "All Item Groups",
-					valuation_rate: 400,
-					stock_uom: "Nos",
-				},
-				true
-			)
 
-			cy.insert_doc(
-				"Customer",
-				{
-					customer_name: "Ankur Agarawal",
-					customer_group: "All Customer Groups",
-					territory: "All Territories",
-				},
-				true
-			)
+		cy.insert_doc(
+			"Item",
+			{
+				item_code: "Sleepy Owl Coffee Mug",
+				item_group: "All Item Groups",
+				valuation_rate: 400,
+				stock_uom: "Nos",
+			},
+			true
+		)
+
+		cy.insert_doc(
+			"Customer",
+			{
+				customer_name: "Ankur Agarawal",
+				customer_group: "All Customer Groups",
+				territory: "All Territories",
+				default_currency: "INR",
+				default_price_list: "Standard Selling",
+			},
+			true
+		)
 	});
 
 	it('Create Sales Invoice', () => {
 		cy.visit('app/sales-invoice');
 		var today = new Date();
 		var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+		const yyyy = today.getFullYear();
+		let mm = today.getMonth() + 1; // Months start at 0!
+		let dd = today.getDate();
+		if (dd < 10) dd = '0' + dd;
+		if (mm < 10) mm = '0' + mm;
+		var today_date = dd + '-' + mm + '-' + yyyy;
+
 		cy.insert_doc(
 			"Sales Invoice",
 			{
@@ -42,23 +51,28 @@ context('Sales Invoice Payment', () => {
 		).then((d)=>{ 
 			console.log(d);
 			cy.visit('app/sales-invoice/'+ d.name);
-			cy.findByRole('button', {name: 'Submit'}).click();
-			cy.findByRole('button', {name: 'Yes'}).click();
-			cy.get('.page-title').should('contain', 'Unpaid');
+			cy.submit('Unpaid');
 
-			cy.findByRole('button', {name: 'Create'}).click();
-			cy.get('[data-label="Payment"]').click();
-			cy.get_field('payment_type', 'Select').should('have.value', 'Receive');
-			cy.get_field('paid_amount', 'Currency').should('have.value', '400.00');
-			//cy.get_table_field('references', 1, 'reference_name', 'Dynamic Link').should('have.value', d.name);
-			cy.get_field('reference_no', 'Data').type('ABC-123');
-			cy.get_field('reference_date', 'Date').click();  //Opens calendar
-			cy.get('.datepicker.active > .datepicker--buttons > .datepicker--button').click();
-			cy.findByRole('button', {name: 'Save'}).click();
-			cy.get('.page-title').should('contain', 'Draft');
-			cy.findByRole('button', {name: 'Submit'}).click();
-			cy.findByRole('button', {name: 'Yes'}).click();
-			cy.get('.page-title').should('contain', 'Submitted');
+			cy.click_dropdown_action('Create', 'Payment');
+			cy.url().should('include', '/app/payment-entry/new-payment-entry');
+			cy.get_select('naming_series').should('have.value', 'ACC-PAY-.YYYY.-');
+			cy.get_select('payment_type').should('have.value', 'Receive');
+			cy.get_input('posting_date').should('have.value', today_date);
+
+			cy.get_input('party_type').should('have.value', 'Customer');
+			cy.get_input('party').should('have.value', 'Ankur Agarawal');
+			cy.get_input('paid_amount').should('have.value', '400.00');
+
+			cy.get_input('references.reference_doctype').should('have.value', 'Sales Invoice');
+			cy.get_input('reference_name').should('have.value', d.name);
+			cy.get_input('allocated_amount').should('have.value', '400.000');
+
+			cy.set_input('reference_no', 'ABC-123');
+			cy.set_today('reference_date');
+
+			cy.save();
+			cy.get_page_title().should('contain', 'Draft');
+			cy.submit('Submitted');
 		});
 	});
 });
