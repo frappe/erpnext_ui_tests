8488fb368eb8e7ac360c1e3f6a0780700561e172
Ankush Menat <ankush@frappe.io>
2022-05-05 12:07:30 +0530

    Revert "test: corrected selectors for pos profile"
    This reverts commit 3b37c987205c998a06357baac0c527fc3156efad.

--- cypress/integration/stock/pos_profile.js
+++ cypress/integration/stock/pos_profile.js
@@ -14,12 +14,12 @@ context('Create POS Profile', () => {
 			cy.get_field('warehouse', 'Link').should('have.value', 'Stores - WP');
 
 			//SeleWP Mode of Payment
-			cy.grid_open_row('payments', 1);
+			cy.get_grid_edit();
 			cy.get_field('default', 'Check').check();
 			cy.get_field('default', 'checkbox').should('be.checked');
 			cy.set_link('payments.mode_of_payment', 'Wire Transfer');
 			cy.get_field('mode_of_payment', 'Link').should('have.value', 'Wire Transfer');
-			cy.close_grid_edit_modal();
+			cy.get('.grid-collapse-row').click();
 
 			//Set POS Configurations
 			cy.get_field('allow_rate_change').check();
