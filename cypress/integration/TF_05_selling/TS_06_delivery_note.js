dcd8bbd5810ce0b7e0f775ca12464d428ac6f04d
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-05-08 16:56:56 +0530

    payment and delivery-removed fixtures

--- cypress/integration/TF_05_selling/TS_06_delivery_note.js
+++ cypress/integration/TF_05_selling/TS_06_delivery_note.js
@@ -1,35 +1,9 @@
 context('Delivery Note Creation', () => {
 	before(() => {
 		cy.login();
-		cy.visit('/app');
-
-		cy.insert_doc(
-			"Item",
-			{
-				item_code: "Solid Wood Dinning Set",
-				item_group: "All Item Groups",
-				valuation_rate: 75000,
-				opening_stock: 5,
-				stock_uom: "Nos",
-			},
-			true
-		)
-
-		cy.insert_doc(
-			"Customer",
-			{
-				customer_name: "William Harris",
-				customer_group: "All Customer Groups",
-				territory: "All Territories",
-				default_currency: "INR",
-				default_price_list: "Standard Selling",
-			},
-			true
-		)
 	});
 
-	it('Create DN via SO>SI', () => {
-		cy.visit('app/sales-order');
+	it('Create DN via SI', () => {
 		var today = new Date();
 		var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 		const yyyy = today.getFullYear();
@@ -39,53 +13,42 @@ context('Delivery Note Creation', () => {
 		if (mm < 10) mm = '0' + mm;
 		var today_date = dd + '-' + mm + '-' + yyyy;
 
-		cy.insert_doc(
-			"Sales Order",
-			{
-				naming_series: "SAL-ORD-.YYYY.-",
-				transaction_date: date,
-				delivery_date: date,
-				customer: "William Harris",
-				order_type: "Sales",
-				items: [{"item_code": "Solid Wood Dinning Set", "delivery_date": date, "qty": 1, "rate": 75000}]
-			},
-			true
-		).then((d)=>{ 
-			console.log(d);
-			cy.visit('app/sales-order/'+ d.name);
-			cy.submit('To Deliver and Bill');
-			cy.click_dropdown_action('Create', 'Sales Invoice');
+		cy.visit('app/sales-invoice');
+		cy.click_listview_row_item(0);
+		cy.get_page_title().should('contain', 'Unpaid');
+		cy.click_dropdown_action('Create', 'Delivery');
+		cy.url().should('include', '/app/delivery-note/new-delivery-note');
 
-			cy.save();
-			cy.get_page_title().should('contain', 'Draft');
-			cy.submit('Unpaid');
-			cy.click_dropdown_action('Create', 'Delivery');
+		cy.get_select('naming_series').should('have.value', 'MAT-DN-.YYYY.-');
+		cy.get_input('customer').should('have.value', 'William Harris');
+		cy.get_input('posting_date').should('have.value', today_date);
 
-			cy.url().should('include', '/app/delivery-note/new-delivery-note');
-			cy.get_select('naming_series').should('have.value', 'MAT-DN-.YYYY.-');
-			cy.get_input('customer').should('have.value', 'William Harris');
-			cy.get_input('posting_date').should('have.value', today_date);
+		cy.click_section('Currency and Price List');
+		cy.get_input('currency').should('have.value', 'INR');
+		cy.get_input('selling_price_list').should('have.value', 'Standard Selling');
 
-			cy.click_section('Currency and Price List');
-			cy.get_input('currency').should('have.value', 'INR');
-			cy.get_input('selling_price_list').should('have.value', 'Standard Selling');
+		cy.get_table_field('items', 1, 'item_code', 'Link').contains('Apple iPhone 13 Pro Max');
+		cy.get_input('items.qty').should('have.value', "1");
+		cy.get_input('rate').should('have.value', "1,10,000.00");
+		cy.get_read_only('amount').should('contain', "1,10,000.00");
 
-			cy.get_table_field('items', 1, 'item_code', 'Link').contains('Solid Wood Dinning Set');
-			cy.get_read_only('total_qty').should('contain', "1");
-			cy.get_read_only('total').should('contain', "₹ 75,000.00");
-			cy.get_read_only('grand_total').should('contain', "₹ 75,000.00");
-			cy.get_read_only('rounded_total').should('contain', "₹ 75,000.00");
+		cy.get_read_only('total_qty').should('contain', "1");
+		cy.get_read_only('total').should('contain', "₹ 1,10,000.00");
+		cy.get_read_only('grand_total').should('contain', "₹ 1,10,000.00");
+		cy.get_read_only('rounded_total').should('contain', "₹ 1,10,000.00");
 
-			cy.save();
-			cy.get_page_title().should('contain', 'Draft');
-			cy.submit('Completed');
-			cy.get_page_title().should('contain', 'William Harris');
+		cy.click_listview_primary_button('Save');
+		cy.get_page_title().should('contain', 'Draft');
+		cy.click_toolbar_button('Submit');
+		cy.click_modal_primary_button('Yes');
+		cy.get_page_title().should('contain', 'Completed');
+		cy.get_page_title().should('contain', 'William Harris');
 
-			cy.click_dropdown_action('View', 'Stock Ledger');
-			cy.get('.dt-cell__content > span > div').should('contain', "-1.000");
+		cy.click_dropdown_action('View', 'Stock Ledger');
+		cy.get('.dt-cell__content > span > div').should('contain', "-1.000");
 
-			cy.visit('app/sales-order/'+ d.name);
-			cy.get_page_title().should('contain', 'Completed');
-		});
+		cy.visit('app/sales-order');
+		cy.click_listview_row_item(0);
+		cy.get_page_title().should('contain', 'Completed');
 	});
 });
