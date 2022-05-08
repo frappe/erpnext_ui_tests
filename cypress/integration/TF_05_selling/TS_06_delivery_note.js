17aa6b1aee0a6a816a82b29611641d928428c6cd
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-05-08 22:35:30 +0530

    hide dialog

--- cypress/integration/TF_05_selling/TS_06_delivery_note.js
+++ cypress/integration/TF_05_selling/TS_06_delivery_note.js
@@ -41,6 +41,7 @@ context('Delivery Note Creation', () => {
 		cy.get_page_title().should('contain', 'Draft');
 		cy.click_toolbar_button('Submit');
 		cy.click_modal_primary_button('Yes');
+		cy.hide_dialog();
 		cy.get_page_title().should('contain', 'Completed');
 		cy.get_page_title().should('contain', 'William Harris');
 
