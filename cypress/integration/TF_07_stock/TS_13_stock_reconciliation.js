d4660dcb27c66ea2f74f354b2d7d1cfd6e3b8423
yadavyk <yashiyadav1998@gmail.com>
2022-05-09 18:01:32 +0530

    test(ui) : stock reconciliation

---
+++ cypress/integration/TF_07_stock/TS_13_stock_reconciliation.js
@@ -0,0 +1,19 @@
+context('Stock Reconciliation', () => {
+	before(() => {
+        cy.login();
+    });
+
+		it("Create a stock reconciliation", () => {
+			cy.new_doc('Stock Reconciliation');
+			cy.set_select('purpose', 'Stock Reconciliation');
+			cy.set_link('items.item_code', 'Teak Shoe Rack');
+			cy.set_link('items.warehouse', 'Stores - WP');
+			cy.set_input('items.qty', '10');
+			cy.set_input('items.valuation_rate', '1200');
+			cy.get_field('expense_account').should('have.value' , 'Stock Adjustment - WP');
+			cy.get_field('cost_center').should('have.value' , 'Main - WP');
+			cy.save();
+			cy.wait(500);
+			cy.submit('Submitted');
+		});
+});
