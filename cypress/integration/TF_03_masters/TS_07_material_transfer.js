cb3b7f0b5a6544ce6313b793c526aacb06c21827
yadavyk <yashiyadav1998@gmail.com>
2022-05-12 10:10:54 +0530

    test(ui) : material transfer for manufacture

---
+++ cypress/integration/TF_03_masters/TS_07_material_transfer.js
@@ -0,0 +1,29 @@
+context('Operations', () => {
+	before(() => {
+        cy.login();
+    });
+
+	it("Start Work Order", () => {
+		cy.visit('/app/work-order');
+		cy.click_listview_row_item(0);
+		cy.click_toolbar_button('Start');
+		cy.on('window:alert',  (str) =>  {
+            expect(str).to.equal(`Select Quantity`)});
+		cy.set_input('qty', '1');
+		cy.click_modal_primary_button('Create');
+		cy.get_field('stock_entry_type').should('have.value', 'Material Transfer for Manufacture');
+		cy.get_field('from_bom', 'checkbox').should('be.checked');
+		cy.get_input('fg_completed_qty', '1');
+		cy.save();
+		cy.wait(500);
+		cy.submit('Submitted');
+
+		//View stock ledger entries are made against correct voucher
+		cy.url().then((url) => {
+			cy.click_dropdown_action('View', 'Stock Ledger')
+			cy.location("pathname").should("eq","/app/query-report/Stock%20Ledger");
+			const name = url.split('/').pop();
+			cy.get_field('voucher_no', 'Data').should('have.value', name);
+		});
+	});
+});
