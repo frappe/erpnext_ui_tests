35b28da700b396777a1729bf24b6b68bd089f049
yadavyk <yashiyadav1998@gmail.com>
2022-05-14 20:17:48 +0530

    changes

--- cypress/integration/TF_07_stock/TS_16_execute_job_cards.js
+++ cypress/integration/TF_07_stock/TS_16_execute_job_cards.js
@@ -14,42 +14,8 @@ context('Executing Job Cards', () => {
 		cy.visit('app/job-card/view/list');
 
 		//Complete first operation
+		cy.clear_filter();
 		cy.click_listview_row_item(0);
-		cy.click_toolbar_button('Start Job');
-		cy.on('window:alert',  (str) =>  {
-			  expect(str).to.equal(`Assign Job to Employee`)});
-		cy.set_input_multiselect('employees', 'John Mayer');
-		cy.click_modal_primary_button('Submit');
-		cy.wait(1000);
-		cy.click_toolbar_button('Complete Job');
-		cy.set_input('qty', '1');
-		cy.click_modal_primary_button('Submit');
-		cy.submit('Completed');
-	});
-
-	it('Complete second operation via Job Cards', () => {
-		cy.visit('app/job-card/view/list');
-
-		//Complete second operation
-		cy.click_listview_row_item(1);
-		cy.click_toolbar_button('Start Job');
-		cy.on('window:alert',  (str) =>  {
-			  expect(str).to.equal(`Assign Job to Employee`)});
-		cy.set_input_multiselect('employees', 'John Mayer');
-		cy.click_modal_primary_button('Submit');
-		cy.wait(1000);
-		cy.click_toolbar_button('Complete Job');
-		cy.set_input('qty', '1');
-		cy.click_modal_primary_button('Submit');
-		cy.click_modal_primary_button('Yes');
-		cy.submit('Completed');
-	});
-
-	it('Complete third operation via Job Cards', () => {
-		cy.visit('app/job-card/view/list');
-
-		//Complete third operation
-		cy.click_listview_row_item(2);
 		cy.click_toolbar_button('Start Job');
 		cy.on('window:alert',  (str) =>  {
 			  expect(str).to.equal(`Assign Job to Employee`)});
