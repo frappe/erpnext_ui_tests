2dab5a301f3e4de038e7f38097f0793be5550ea5
yadavyk <yashiyadav1998@gmail.com>
2022-05-14 12:19:45 +0530

    changes

--- cypress/integration/TF_07_stock/TS_16_execute_job_cards.js
+++ cypress/integration/TF_07_stock/TS_16_execute_job_cards.js
@@ -10,7 +10,7 @@ context('Executing Job Cards', () => {
 		cy.get_read_only('material_transferred_for_manufacturing').should('contain','1');
 	});
 
-	it('Complete Operations via Job Cards', () => {
+	it('Complete first operation via Job Cards', () => {
 		cy.visit('app/job-card/view/list');
 
 		//Complete first operation
@@ -25,6 +25,10 @@ context('Executing Job Cards', () => {
 		cy.set_input('qty', '1');
 		cy.click_modal_primary_button('Submit');
 		cy.submit('Completed');
+	});
+
+	it('Complete second operation via Job Cards', () => {
+		cy.visit('app/job-card/view/list');
 
 		//Complete second operation
 		cy.click_listview_row_item(1);
@@ -39,6 +43,10 @@ context('Executing Job Cards', () => {
 		cy.click_modal_primary_button('Submit');
 		cy.click_modal_primary_button('Yes');
 		cy.submit('Completed');
+	});
+
+	it('Complete third operation via Job Cards', () => {
+		cy.visit('app/job-card/view/list');
 
 		//Complete third operation
 		cy.click_listview_row_item(2);
@@ -52,26 +60,5 @@ context('Executing Job Cards', () => {
 		cy.set_input('qty', '1');
 		cy.click_modal_primary_button('Submit');
 		cy.submit('Completed');
-	});
-
-	it('Finish work order', () => {
-		cy.click_listview_row_item(0);
-  		cy.click_toolbar_button('Finish');
-  		cy.on('window:alert',  (str) =>  {
-	   		expect(str).to.equal(`Select Quantity`)});
-  		cy.set_input('qty', '1');
-  		cy.click_modal_primary_button('Create');
-  		cy.get_field('stock_entry_type').should('have.value', 'Manufacture');
-  		cy.get_field('from_bom', 'checkbox').should('be.checked');
-	  	cy.get_input('fg_completed_qty', '1');
-	  	cy.save();
-	  	cy.wait(500);
-	  	cy.submit('Submitted');
-	});
-
-	it('Check status for work order', () => {
-		cy.click_listview_row_item(0);
-  		cy.get_page_title().should('contain', 'Completed');
-		cy.get_read_only('produced_qty').should('contain','1');
 	});
 });
