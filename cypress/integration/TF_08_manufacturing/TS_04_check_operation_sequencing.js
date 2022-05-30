51fc10e3ee424c9b4248511cc40882b150a129d6
yadavyk <yashiyadav1998@gmail.com>
2022-05-30 13:24:48 +0530

    changes

--- cypress/integration/TF_08_manufacturing/TS_04_check_operation_sequencing.js
+++ cypress/integration/TF_08_manufacturing/TS_04_check_operation_sequencing.js
@@ -8,17 +8,29 @@ context('Work Order', () => {
 		cy.click_listview_row_item(0);
 		cy.click_dropdown_action('Create', 'Work Order');
 		cy.on('window:alert',  (str) =>  {
-            expect(str).to.equal(`Work Order`)});
+			expect(str).to.equal(`Work Order`)});
 		cy.set_input('qty', '1');
 		cy.click_modal_primary_button('Create');
 		cy.get_field('production_item').should('have.value', 'Study Table');
 		cy.get_input('qty', '1');
-		cy.set_link('source_warehouse', 'Stores - WP');
-		cy.set_link('fg_warehouse', 'Finished Goods - WP');
-		cy.set_link('wip_warehouse', 'Work In Progress - WP');
+		cy.set_link('source_warehouse', 'Stores - CT');
+		cy.set_link('fg_warehouse', 'Finished Goods - CT');
+		cy.set_link('wip_warehouse', 'Work In Progress - CT');
 
 		//Setting operations against raw materials
 		cy.grid_open_row('required_items', 1);
+		cy.set_link('operation', 'Attaching the table top with legs');
+		cy.close_grid_edit_modal();
+		cy.grid_open_row('required_items', 2);
+		cy.set_link('operation', 'Attaching the table top with legs');
+		cy.close_grid_edit_modal();
+		cy.grid_open_row('required_items', 3);
+		cy.set_link('operation', 'Attaching the table top with legs');
+		cy.close_grid_edit_modal();
+		cy.grid_open_row('required_items', 4);
+		cy.set_link('operation', 'Sanding  the table');
+		cy.close_grid_edit_modal();
+		cy.grid_open_row('required_items', 5);
 		cy.set_link('operation', 'Staining the table');
 		cy.close_grid_edit_modal();
 		cy.save();
@@ -32,23 +44,21 @@ context('Work Order', () => {
 		cy.get_page_title().should('contain', 'Open');
 	});
 
-	it('Start operation via Job Card', () => {
+	it('Start an operation randomly via Job Card', () => {
 		cy.visit('app/job-card/view/list');
 		cy.click_listview_row_item(0);
-		/*cy.click_toolbar_button('Start Job');
+		cy.click_toolbar_button('Start Job');
 		cy.on('window:alert',  (str) =>  {
 			  expect(str).to.equal(`Assign Job to Employee`)});
-		cy.set_input_multiselect('employees', 'John Mayer');*/
+		cy.set_input_multiselect('employees', 'John Mayer');
 		cy.click_modal_primary_button('Submit');
 		cy.click_section('Production');
 		cy.get_input('for_quantity', '1');
-		cy.get_field('wip_warehouse').should('have.value', 'Work In Progress - WP')
+		cy.get_field('wip_warehouse').should('have.value', 'Work In Progress - CT')
 	});
 
-	it('Check validation for sequencing of operations', () => {
+	it('Check validation for operation sequencing', () => {
 		cy.visit('app/job-card/view/list');
-
-		//Complete first operation
 		cy.click_listview_row_item(0);
 		cy.click_toolbar_button('Complete Job');
 		cy.set_input('qty', '1');
