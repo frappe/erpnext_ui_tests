504210d72000705460777cd0451fa2e3700cdd8d
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-05-31 12:21:58 +0530

    fix: Correcting failed tests

--- cypress/integration/TF_02_framework/TS_10_role_profile.js
+++ cypress/integration/TF_02_framework/TS_10_role_profile.js
@@ -21,14 +21,11 @@ context('Role Profile', () => {
 		cy.set_input('first_name', 'Test Role User');
 		cy.click_modal_primary_button('Save');
 		cy.wait(2000);
-		//cy.hide_dialog();
-		//cy.get('.btn-modal-close').click({force: true, multiple: true});
-		//cy.get('.modal').type('{esc}');
 		cy.get('.modal-actions button.btn-modal-close').click({force: true, multiple: true});
 		cy.wait(1000);
 
 		//Checking if the roles selected in role profile is also checked in the user
-		cy.get_field('role_profile_name', 'Link').scrollIntoView().should('have.value', 'Test RoleProfile');
+		cy.get_field('role_profile_name', 'Link').should('have.value', 'Test RoleProfile');
 		cy.get_field('roles', 'MultiCheck').should('be.checked');
 	});
 
@@ -40,7 +37,9 @@ context('Role Profile', () => {
 		cy.save();
 		cy.wait(1000);
 		cy.get_field('role_profile_name', 'Link').click({force: true, scrollBehavior: false});
-		cy.get('[data-fieldname="role_profile_name"] ul:visible li:first-child').should('contain', 'Test RoleProfile');	
+		cy.get('[data-fieldname="role_profile_name"] ul:visible li:first-child')
+			.should('contain', 'Test RoleProfile')
+			.click({force:true});	
 		cy.get_field('roles', 'MultiCheck').should('be.checked');
 	});
 
@@ -65,26 +64,21 @@ context('Role Profile', () => {
 		cy.wait(1000);
 
 		//Checking if the roles selected in role profile is also checked in the user
-		cy.get_field('role_profile_name', 'Link').scrollIntoView().should('have.value', 'Test RoleProfile1');
+		cy.get_field('role_profile_name', 'Link').should('have.value', 'Test RoleProfile1');
 		cy.get('.role-editor [type="checkbox"][data-unit="System Manager"]:visible').should('be.checked');
 		cy.get('.role-editor [type="checkbox"][data-unit="Sales Manager"]:visible').should('be.checked');
 
 		//Deleting the user and the role profile
-		cy.delete_first_record('user');
-		cy.hide_dialog();
-		cy.delete_first_record('role profile');
-		cy.hide_dialog();
+		cy.remove_doc('User', 'test_rle_user@example.com');
+		cy.remove_doc('Role Profile', 'Test RoleProfile1');
 	});
 
-	it('Deleting user and role profile', () => {
+	after(() => {
         //Deleting the user
-        cy.delete_first_record('user');
-        cy.hide_dialog();
-		cy.delete_first_record('user');
-		cy.hide_dialog();
+		cy.remove_doc('User', 'test_role_user123@exapmle.com');
+		cy.remove_doc('User', 'test_role_user@exapmle.com');
 
 		//Deleting the role profile
-		cy.delete_first_record('role profile');
-		cy.hide_dialog();
+		cy.remove_doc('Role Profile', 'Test RoleProfile');
     });
 });
