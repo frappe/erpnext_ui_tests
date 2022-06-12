b98fdca72fdf53fe1958907e286e81b75b951c76
yadavyk <yashiyadav1998@gmail.com>
2022-06-12 08:54:40 +0530

    changes

--- cypress/integration/TF_07_stock/TS_19_quality_inspection.js
+++ cypress/integration/TF_07_stock/TS_19_quality_inspection.js
@@ -13,6 +13,7 @@ context("Quality Inspection", () => {
 		cy.set_input('standard_rate', '11999');
 		cy.set_link('stock_uom', 'Nos');
 		cy.click_section('Inspection Criteria');
+		cy.open_section('Inspection Criteria');
 		cy.set_link('quality_inspection_template', 'Quality Inspection Template - I');
 		cy.get_field('inspection_required_before_delivery').check();
 		cy.save();
