8778aa00d8e15a9ee5c45aa5691bedd04d0452ea
yadavyk <yashiyadav1998@gmail.com>
2022-06-14 14:32:38 +0530

    test : complete production operations via job card

---
+++ cypress/integration/TF_08_manufacturing/TS_07_job_card_execution.js
@@ -0,0 +1,35 @@
+context('Executing Job Cards', () => {
+	before(() => {
+        cy.login();
+    });
+
+	it('Check status of Work Order', () => {
+		cy.visit('/app/work-order');
+		cy.click_listview_row_item(0);
+		cy.get_page_title().should('contain', 'In Process');
+		cy.get_read_only('material_transferred_for_manufacturing').should('contain','1');
+	});
+
+	it('Start operation via Job Card', () => {
+		cy.visit('app/job-card/view/list');
+		cy.click_listview_row_item(0);
+		cy.click_toolbar_button('Start Job');
+		cy.on('window:alert',  (str) =>  {
+			  expect(str).to.equal(`Assign Job to Employee`)});
+		cy.set_input_multiselect('employees', 'John Mayer');
+		cy.click_modal_primary_button('Submit');
+		cy.click_section('Production');
+		cy.open_section('Production');
+		cy.get_input('for_quantity', '1');
+		cy.get_field('wip_warehouse').should('have.value', 'Work In Progress - WP')
+	});
+
+	it('Complete operation via Job Cards', () => {
+		cy.visit('app/job-card/view/list');
+		cy.click_listview_row_item(0);
+		cy.click_toolbar_button('Complete Job');
+		cy.set_input('qty', '1');
+		cy.click_modal_primary_button('Submit');
+		cy.submit('Completed');
+	});
+});
