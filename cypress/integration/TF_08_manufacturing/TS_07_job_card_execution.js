b3514a376f26c9ba3c9b1c6c120efde5890643d2
yadavyk <yashiyadav1998@gmail.com>
2022-06-15 10:27:53 +0530

    changes

--- cypress/integration/TF_08_manufacturing/TS_07_job_card_execution.js
+++ cypress/integration/TF_08_manufacturing/TS_07_job_card_execution.js
@@ -1,35 +1,32 @@
-context('Executing Job Cards', () => {
+context('Finish work order', () => {
 	before(() => {
         cy.login();
     });
 
-	it('Check status of Work Order', () => {
+	it('Make manufacture entry for finished good and scrap item', () => {
 		cy.visit('/app/work-order');
-		cy.click_listview_row_item(0);
-		cy.get_page_title().should('contain', 'In Process');
-		cy.get_read_only('material_transferred_for_manufacturing').should('contain','1');
-	});
+        cy.click_listview_row_item(0);
+        cy.click_toolbar_button('Finish');
+        cy.on('window:alert',  (str) =>  {
+            expect(str).to.equal(`Select Quantity`)});
+        cy.set_input('qty', '1');
+        cy.click_modal_primary_button('Create');
+        cy.get_field('stock_entry_type').should('have.value', 'Manufacture');
+        cy.get_input('fg_completed_qty', '1');
+        cy.save();
+        cy.wait(500);
+		cy.grid_open_row('items', 3);
+		cy.get_field('t_warehouse').should('have.value' , 'Finished Goods - WP');
+		cy.get_field('item_code').should('have.value' , 'WB-001');
+		cy.get_input('is_scrap_item', 'checkbox').should('be.checked');
+		cy.get_input('qty').should('have.value', '1.000');
+		cy.close_grid_edit_modal();
+        cy.submit('Submitted');
+    });
 
-	it('Start operation via Job Card', () => {
-		cy.visit('app/job-card/view/list');
-		cy.click_listview_row_item(0);
-		cy.click_toolbar_button('Start Job');
-		cy.on('window:alert',  (str) =>  {
-			  expect(str).to.equal(`Assign Job to Employee`)});
-		cy.set_input_multiselect('employees', 'John Mayer');
-		cy.click_modal_primary_button('Submit');
-		cy.click_section('Production');
-		cy.open_section('Production');
-		cy.get_input('for_quantity', '1');
-		cy.get_field('wip_warehouse').should('have.value', 'Work In Progress - WP')
-	});
-
-	it('Complete operation via Job Cards', () => {
-		cy.visit('app/job-card/view/list');
-		cy.click_listview_row_item(0);
-		cy.click_toolbar_button('Complete Job');
-		cy.set_input('qty', '1');
-		cy.click_modal_primary_button('Submit');
-		cy.submit('Completed');
-	});
+    it('Check status for work order', () => {
+        cy.click_listview_row_item(0);
+        cy.get_page_title().should('contain', 'Completed');
+        cy.get_read_only('produced_qty').should('contain','1');
+    });
 });
