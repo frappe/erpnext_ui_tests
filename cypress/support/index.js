e01332c3eccd5a96fe8eb46ddbff254b58bf5510
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-06-16 14:43:57 +0530

    fix: removing uncaugth exception section

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -25,12 +25,6 @@ Cypress.Cookies.defaults({
 	preserve: "sid",
 });
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-});
-
 // spy on error and warnings
 Cypress.on('window:before:load', (win) => {
   cy.spy(win.console, 'error');
