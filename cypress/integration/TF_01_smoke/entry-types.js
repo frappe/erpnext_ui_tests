11eb06a555d606c593b6a2c3d911b079d7c09ae1
Komal-Saraf0609 <komallsaraf0609@gmail.com>
2022-06-21 18:39:15 +0530

    fix: fixing tests

--- cypress/integration/TF_01_smoke/entry-types.js
+++ cypress/integration/TF_01_smoke/entry-types.js
@@ -83,7 +83,7 @@ context("Journal Entry Types", () => {
         cy.grid_open_row('accounts', 3);
         cy.get_input('account').click({force: true});
         cy.click_link_button();
-        cy.get_read_only('root_type').should('contain', 'Asset');
+        cy.get_read_only('root_type').should('contain', 'Liability');
         cy.go('back');
         cy.grid_open_row('accounts', 18);
         cy.get_input('account').click({force: true});
