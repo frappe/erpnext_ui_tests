ebb513f84c47340032e7640dbc665a62a43e3b8b
yadavyk <yashiyadav1998@gmail.com>
2022-06-22 18:23:42 +0530

    changes

--- cypress/integration/TF_04_accounts/TS_12_pos_profile.js
+++ cypress/integration/TF_04_accounts/TS_12_pos_profile.js
@@ -14,12 +14,6 @@ context('Create POS Profile', () => {
 			cy.set_link('warehouse', 'Stores - WP');
 			cy.get_field('warehouse', 'Link').should('have.value', 'Stores - WP');
 
-			cy.grid_add_row('applicable_for_users');
-			cy.grid_open_row('applicable_for_users', 1);
-			cy.get_field('default', 'Check').check();
-			cy.get_field('default', 'checkbox').should('be.checked');
-			cy.close_grid_edit_modal();
-
 			//Select Mode of Payment
 			cy.grid_open_row('payments', 1);
 			cy.get_field('default', 'Check').check();
