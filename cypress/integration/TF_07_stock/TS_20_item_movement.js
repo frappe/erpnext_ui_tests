d1b8c6e0749856fcbbb1220431b5280b403c948b
yadavyk <yashiyadav1998@gmail.com>
2022-07-05 15:41:27 +0530

    changes to warehouse

--- cypress/integration/TF_07_stock/TS_20_item_movement.js
+++ cypress/integration/TF_07_stock/TS_20_item_movement.js
@@ -9,18 +9,18 @@ context("Item", () => {
 		cy.findByRole('button', {name: /Move/i}).click();
 		cy.on('window:alert',  (str) =>  {
         	expect(str).to.equal(`Move Item`)});
-		cy.set_link('target', 'Work In Progress - CT');
+		cy.set_link('target', 'Work In Progress - WP');
 		cy.get_field('qty').clear();
 		cy.set_input('qty', '5');
 		cy.click_modal_primary_button('Create Stock Entry');
 		cy.url().should('include', '/app/stock-entry/new-stock-entry');
 		cy.get_input('stock_entry_type', 'Material Transfer');
-		cy.get_input('from_warehouse', 'Stores - CT');
-		cy.get_input('to_warehouse', 'Work In Progress - CT');
+		cy.get_input('from_warehouse', 'Stores - WP');
+		cy.get_input('to_warehouse', 'Work In Progress - WP');
 		cy.grid_open_row('items', 1);
 		cy.get_input('item_code', 'Study Table');
-		cy.get_input('s_warehouse', 'Stores - CT');
-		cy.get_input('t_warehouse', 'Work In Progress - CT');
+		cy.get_input('s_warehouse', 'Stores - WP');
+		cy.get_input('t_warehouse', 'Work In Progress - WP');
 		cy.set_input('qty', 5);
 		cy.close_grid_edit_modal();
 		cy.save();
