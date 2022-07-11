495de162a8cc0201b4b70d6654a4d935a1650693
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-07-11 13:04:37 +0530

    fix: change in naming series fix

--- cypress/integration/TF_04_accounts/TS_11_discount_on_price_list.js
+++ cypress/integration/TF_04_accounts/TS_11_discount_on_price_list.js
@@ -62,7 +62,7 @@ context('Discount Check on Price List Rate', () => {
 		cy.new_doc('Sales Invoice');
 		cy.location('pathname').should('include', '/app/sales-invoice/new-sales-invoice');
 
-		cy.get_select('naming_series').should('have.value', 'SINV-.YY.-');
+		cy.get_select('naming_series').should('have.value', 'ACC-SINV-.YYYY.-');
 		cy.get_input('company').should('not.have.value', 0);
 		cy.get_input('posting_date').should('not.have.value', 0);
 		cy.wait(500);
