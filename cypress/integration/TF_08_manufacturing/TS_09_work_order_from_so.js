d03efbdfbe32ab176bcaf505a08147f4198c9283
yadavyk <yashiyadav1998@gmail.com>
2022-07-19 11:38:56 +0530

    changes

--- cypress/integration/TF_08_manufacturing/TS_09_work_order_from_so.js
+++ cypress/integration/TF_08_manufacturing/TS_09_work_order_from_so.js
@@ -7,6 +7,7 @@ context('Create Sales Order', () => {
 		cy.new_doc("Sales Order");
 		cy.url().should('include', '/app/sales-order/new-sales-order');
 		cy.get_select('naming_series').should('have.value', 'SAL-ORD-.YYYY.-');
+		cy.set_link('company', 'Wind Power LLC')
 		cy.set_link('customer', 'William Harris');
 		cy.get_input('customer').should('have.value', 'William Harris');
 		cy.get_select('order_type').should('have.value', 'Sales');
@@ -55,7 +56,7 @@ context('Create Sales Order', () => {
  		cy.get_error_msg().should('contain', 'Mandatory fields required in Work Order');
 		cy.hide_dialog();
 		cy.set_link('wip_warehouse', 'Work In Progress - WP');
-		cy.click_toolbar_button('Save');
+		cy.click_toolbar_button('Submit');
 		cy.on('window:alert',  (str) =>  {
             expect(str).to.equal(`Missing Fields`)});
 		cy.hide_dialog();
