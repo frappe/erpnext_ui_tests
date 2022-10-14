02434138a4ea91c29c7e67d5e98365088f87a2ea
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-10-14 18:30:45 +0530

    more fixes

--- cypress/integration/TF_05_selling/TS_09_fetch_terms_and_conditions.js
+++ cypress/integration/TF_05_selling/TS_09_fetch_terms_and_conditions.js
@@ -17,11 +17,15 @@ context('Fetching Terms and Conditions in Quotation', () => {
 		cy.get_select('order_type').should('have.value', 'Sales');
 		cy.get_read_only('customer_name').should('contain', 'William Harris');
 
-		cy.open_section('Address and Contact');
+		//cy.open_section('Address and Contact');
+		cy.findByRole("tab", { name: "Address & Contact" }).click();
 		cy.get_read_only('customer_address').should('contain', "William's Address-Billing");
 		cy.get_read_only('shipping_address_name').should('contain', "William's Address-Billing");
+		cy.findByRole("tab", { name: "More Info" }).click();
+		cy.click_section_head('additional_info_section');
 		cy.get_read_only('territory').should('contain', 'All Territories');
 
+		cy.click_tab('Details');
 		cy.set_link('items.item_code', 'Apple iPhone 13 Pro Max');
 		cy.get_input('qty').should('have.value', "1.000");
 		cy.get_input('rate').clear();
@@ -34,6 +38,7 @@ context('Fetching Terms and Conditions in Quotation', () => {
 		cy.get_read_only('grand_total').should('contain', "₹ 1,10,000.00");
 		cy.get_read_only('rounded_total').should('contain', "₹ 1,10,000.00");
 
+		cy.click_tab('Terms');
 		cy.findByText('Terms and Conditions').scrollIntoView().should('be.visible');
 		cy.click_section('Terms and Conditions');
 		cy.set_link('tc_name', 'Standard Terms and Condition');
