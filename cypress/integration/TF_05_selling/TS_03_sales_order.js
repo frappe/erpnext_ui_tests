08f1476de9f2cce7dd042bb2861c4b3f4ef3edca
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-10-21 14:19:12 +0530

    so-fix

--- cypress/integration/TF_05_selling/TS_03_sales_order.js
+++ cypress/integration/TF_05_selling/TS_03_sales_order.js
@@ -41,6 +41,8 @@ context('Create Sales Order', () => {
 		cy.get_read_only('rounded_total').should('contain', "₹ 1,10,000.00");
 
 		cy.click_toolbar_button('Save');
+		cy.visit('/app/quotation');
+		cy.click_listview_row_item(0);
 		cy.get_page_title().should('contain', 'Draft');
 		cy.click_toolbar_button('Submit');
 		cy.click_modal_primary_button('Yes');
