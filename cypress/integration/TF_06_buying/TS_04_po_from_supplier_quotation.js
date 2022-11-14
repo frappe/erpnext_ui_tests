341bf81155a97d5cd9b99f8d09d079cbb47da40b
Nidhi Purohit <nidhi1105purohit@gmail.com>
2022-11-14 15:19:50 +0530

    po changes

--- cypress/integration/TF_06_buying/TS_04_po_from_supplier_quotation.js
+++ cypress/integration/TF_06_buying/TS_04_po_from_supplier_quotation.js
@@ -18,19 +18,29 @@ context('Purchase Order from Supplier Quotation', () => {
 		cy.set_link('set_warehouse', 'Stores - WP');
 
 		cy.get_input('items.item_code').should('have.value', 'Apple Macbook Pro 16 inch');
-		cy.get_input('item.schedule_date').should('not.have.value', 0);
-		cy.get_input('qty').should('have.value', "10");
+		cy.get_input('schedule_date').should('not.have.value', 0);
+		cy.get_input('qty').should('have.value', "10.000");
+		cy.get_input('qty').focus().clear();
+		cy.wait(200);
+		cy.set_input('qty', '1');
+		cy.get_input('qty').blur();
 		cy.get_input('uom').should('have.value', "Nos");
 		cy.get_input('rate').should('have.value', "2,50,000.00");
-		cy.get_read_only('amount').should('contain', "25,00,000.00");
+		cy.get_read_only('amount').should('contain', "2,50,000.00");
 
-		cy.get_read_only('total_qty').should('contain', "10");
-		cy.get_read_only('total').should('contain', "₹ 25,00,000.00");
-		cy.get_read_only('grand_total').should('contain', "₹ 25,00,000.00");
-		cy.get_read_only('rounded_total').should('contain', "₹ 25,00,000.00");
+		cy.get_read_only('total_qty').should('contain', "1");
+		cy.get_read_only('total').should('contain', "₹ 2,50,000.00");
+		cy.get_read_only('grand_total').should('contain', "₹ 2,50,000.00");
+		cy.get_read_only('rounded_total').should('contain', "₹ 2,50,000.00");
 
 		cy.save();
-		// cy.get_page_title().should('contain', 'Draft');
-		// cy.submit_doc('Submitted');
+		cy.get_page_title().should('contain', 'Draft');
+		cy.click_toolbar_button('Submit');
+		cy.click_modal_primary_button('Yes');
+		cy.get_page_title().should('contain', 'To Receive and Bill');
+
+		cy.visit('app/material-request/');
+		cy.click_listview_row_item(0);
+		cy.get_page_title().should('contain', 'Partially ordered');
 	});
 });
